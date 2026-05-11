21eb76a1c55555ce499c4f7070eb0891bf583a16
김은서 <fpv52m7ztt@privaterelay.appleid.com>
2026-05-12 01:03:19 +0900

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+function App() {
+  return (
+    <div>
+      <h1>고독사 예방 AI 서비스</h1>
+      <p>
+        1인 가구의 이상 징후를 조기 감지하고 보호자와 복지 담당자에게
+        알림을 보내는 웹 서비스입니다.
+      </p>
+
+      <button>시작하기</button>
+    </div>
+  )
+}
+
+export default App
